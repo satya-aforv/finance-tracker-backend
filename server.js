a88a1a94e765c2bfc31bdfec02197291242b4b47
“Satyabrata <Satyasahoo525@gmail.com>
2025-07-22 10:48:55 +0530

    added

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ const corsOptions = {
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://finance-tracker-chi-three.vercel.app",
     process.env.FRONTEND_URL,
   ].filter(Boolean),
   credentials: true,
